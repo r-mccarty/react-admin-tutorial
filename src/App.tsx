62826540669cc00eef0b52c640fc82c413bdf422
r-mccarty <ry.mccarty13@gmail.com>
2025-04-14 16:32:29 -0700

    replaced ListGuesser

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+
 import { dataProvider } from './dataProvider';
+import { Admin, Resource } from "react-admin";
+import { UserList } from "./users";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-     <Resource name="users" list={ListGuesser} />
+     <Resource name="users" list={UserList} />
   </Admin>
 );
