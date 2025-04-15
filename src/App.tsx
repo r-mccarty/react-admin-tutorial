0be432be5812a08f2032d6965aa783a44b918b2f
r-mccarty <ry.mccarty13@gmail.com>
2025-04-14 18:04:40 -0700

    added Posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import { dataProvider } from './dataProvider';
-import { Admin, Resource } from "react-admin";
+import { Admin, Resource} from "react-admin";
 import { UserList } from "./users";
+import { PostList } from "./posts";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
+     <Resource name="posts" list={PostList} />
      <Resource name="users" list={UserList} />
   </Admin>
 );
